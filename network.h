2e2216d3d99418061af3e4219c27c5aede9d7e8c
Justin Holmgren <justin.holmgren@gmail.com>
2012-11-26 10:59:57 -0500

    added some missing files

---
+++ network.h
@@ -0,0 +1,25 @@
+#ifndef _DSM_NETWORK_H
+#define _DSM_NETWORK_H
+#define __STDC_FORMAT_MACROS
+
+#define MAXBUFLEN 5000
+#include "copyset.h"
+#include "messages.h"
+
+struct PortInfo {
+  int info_port;
+  int req_port;
+};
+
+extern char *ips[];
+extern struct PortInfo ports[];
+
+int open_serv_socket(int port);
+int open_socket(char *host, int port);
+
+struct PageInfoMessage *recvPgInfoMsg(int sockfd);
+struct RequestPageMessage *recvReqPgMsg(int sockfd);
+int sendReqPgMsg(struct RequestPageMessage *msg, client_id_t id);
+int sendPgInfoMsg(struct PageInfoMessage *msg, client_id_t id);
+
+#endif

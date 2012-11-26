00de66170e9f4569242f51af580847e9f0e4a565
Justin Holmgren <justin.holmgren@gmail.com>
2012-11-26 11:41:32 -0500

    stupid formatting stuff

--- libdsm.c
+++ libdsm.c
@@ -87,7 +87,7 @@ void process_write_request(void * addr, client_id_t requester) {
 }
 
 
-/** Check if it's a write fault or read fault: returns 1 if write fault*/
+/** Check if it's a write fault or read fault: returns 1 if write fault */
 int is_write_fault(int signum, siginfo_t *info, void *ucontext) {
     // !! normalizes to 0 or 1
     return !!(((ucontext_t *) ucontext)->uc_mcontext.gregs[REG_ERR] & PTE_W); 
